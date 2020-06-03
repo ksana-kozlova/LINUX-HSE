3a56733387732f5b3814bc93a673a58086eb86bc
kozlova_oksana <kozlova.ksucha@yandex.ru>
2020-06-03 11:32:34 +0300

    daemon

--- daemon.c
+++ daemon.c
@@ -5,163 +5,65 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdbool.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <semaphore.h>
 
 bool coming_signal = false;
 bool program_exit = false;
-bool child_signal = false;
 
-sem_t sem;
-
-void sigint_handler(int signum) {
+void sigint_handler(int signum){
 	coming_signal = true;
 }
 
-void sigterm_handler(int signum) {
+void sigterm_handler(int signum){
 	program_exit = true;
 }
 
-void sigchld_handler(int signum) {
-	child_signal = true;
-}
 
-
-void writeLogMsg(char *message)
-{
-	int fd = open("log.txt", O_CREAT | O_RDWR | O_APPEND, S_IRWXU);
-	char fullMsg[512];
-	char *autor = "DAEMON: ";
-
-	strcpy(fullMsg, autor);
-	strcat(fullMsg, message);
-	strcat(fullMsg, "\n");
-
-	write(fd, fullMsg, strlen(fullMsg));
-
-	close(fd);
-}
-
-int commandExecuter(char **args)
-{
-	int output = open("output.txt", O_CREAT | O_RDWR | O_APPEND, S_IRWXU);
-
-	dup2(output, 1);
-	execv(args[0], args);
-
-}
-
-
-int Daemon(char** argv) {
+int Daemon(char** argv){
 	signal(SIGINT, sigint_handler);
 	signal(SIGTERM, sigterm_handler);
-	signal(SIGCHLD, sigchld_handler);
 
-	if (sem_init(&sem, 0, 1) == -1)
-	{
-		writeLogMsg("Error, can't to initialize the semaphore");
-		program_exit = true;
-		writeLogMsg("Daemon ended work because of error");
-	}
+	char buf_exit[] = "I am daemn.I am ending\n";
 
-	char buf[4096];
+	char buf[256];
 	int fd_read = open(argv[1], O_RDWR, S_IRWXU);
-	int count = read(fd_read, buf, sizeof(buf));
+	int count = read(fd_read,buf, sizeof(buf));
 	close(fd_read);
-	writeLogMsg("Start!");
-	while (!program_exit) {
-		pause();
-		if (coming_signal) {
-			writeLogMsg("Signal has comed");
-			if (count > 0) {
-				char *commands[64];
-				int cnt = 0;
-				char *tmp;
-				tmp = strtok(buf, "\n");
-				while (tmp != NULL) {
-					commands[cnt++] = tmp;
-					tmp = strtok(NULL, "\n");
-				}
+	buf[count-1] = '\0';
+	
+	int fd_write = open("/tmp/output.txt", O_CREAT|O_RDWR, S_IRWXU);	
+	close(1);
+	dup2(fd_write, 1);
 
-				for (int i = 0; i < cnt; i++) {
-					pid_t pid;
-					if ((pid = fork()) == 0) {
-
-						char *args[64];
-						int cntArg = 0;
-						char *tmpArg;
-
-						tmpArg = strtok(commands[i], " ");
-						args[cntArg++] = tmpArg;
-
-						while (tmpArg != NULL) {
-							tmpArg = strtok(NULL, " ");
-							args[cntArg++] = tmpArg;
-
-						}
-
-						args[cntArg] = NULL;
-
-						if (sem_wait(&sem) == -1) {
-							writeLogMsg("Error while sem_wait operation!");
-						}
-						else {
-							char msg[100] = "Command is executed:";
-							strcat(msg, args[0]);
-							writeLogMsg(msg);
-							commandExecuter(args);
-
-						}
-
-					}
-					else if (pid > 0) {
-						while (1) {
-							if (child_signal) {
-								waitpid(-1, NULL, 0);
-								sem_post(&sem);
-								child_signal = false;
-								break;
-							}
-							pause();
-						}
-					}
-				}
+	lseek(fd, 0, SEEK_END); 
+	
+	while(!program_exit){
+		if(coming_signal){
+			pid_t parpid;
+			if((parpid=fork()) == 0){       
+		      		execve(buf, argv+1, NULL);         
 			}
-			else {
-				writeLogMsg("count == 0, error read file");
-			}
-
 			coming_signal = false;
 		}
-
-	}
-
-	sem_destroy(&sem);
-	writeLogMsg("Finish!");
-
-
+	}	
+	write(fd_write, buf_exit, sizeof(buf_exit));
+	
 	return 0;
 }
-
-
 
 int main(int argc, char* argv[])
 {
-
 	pid_t parpid;
-	if ((parpid = fork()) < 0) {
-		printf("\ncan't fork");
-		exit(1);
+	if((parpid=fork())<0) 
+	{                
+   		printf("\ncan't fork");
+      		exit(1);           
 	}
-	else if (parpid != 0)
-		exit(0);
+       	else if (parpid!=0)
+        	exit(0);             
 	printf("I am daemon.pid = %i\n", getpid());
 	setsid();
-	close(1);
-	Daemon(argv);
-	return 0;
+	Daemon(argv);        
+        return 0;
 }
 
 
